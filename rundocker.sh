dc0357bb023e175d31771039efda2ff03604c5da
giuliano giusti <giuliano.giusti61@gmail.com>
2023-02-27 22:12:56 +0100

    Aggiornamento Mon Feb 27 22:12:56 CET 2023

--- rundocker.sh
+++ rundocker.sh
@@ -5,6 +5,7 @@ docker image prune -f
 docker container prune -f
 docker run -t --name=NN --rm -v $PWD/app/db:/home/node/app/db -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run.sh
 ./sync.sh
+set -e
 for i in 1 2 3 4 5 6 7 8 9 10
 do
 docker run -t --name=NN --rm -v $PWD/app/db:/home/node/app/db -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run-2.sh
