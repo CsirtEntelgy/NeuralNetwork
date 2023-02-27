1a5745527b9958355a3f812e39e9ec646922006a
giuliano giusti <giuliano.giusti61@gmail.com>
2023-02-27 22:11:41 +0100

    Aggiornamento Mon Feb 27 22:11:41 CET 2023

--- app/bin/run-2.sh
+++ app/bin/run-2.sh
@@ -4,8 +4,9 @@ node src/impara-db.js 10 2022-31-12
 for CHIAVE in ultimoValore variazionePercentuale
 do for ID_AZIENDA in {1..3000}
    do if [ -e dati/elaborazioni/elab.${CHIAVE}.$ID_AZIENDA.dat ]
+      then
       (
-        echo "delete from iterazioni where id_azienda=$ID_AZIENDA and chiave='$CHIAVE' " 
+        echo "delete from iterazioni where id_azienda=$ID_AZIENDA and chiave='$CHIAVE';" 
         echo ".import dati/elaborazioni/elab.${CHIAVE}.$ID_AZIENDA.dat iterazioni"
       ) |sqlite3 db/elaborazioni.db
       rm  dati/elaborazioni/elab.${CHIAVE}.$ID_AZIENDA.dat
