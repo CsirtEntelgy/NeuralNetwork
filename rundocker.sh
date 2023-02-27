31f84620281cd08af6710322d375116f7f901745
giuliano.giusti@unibo.it <giuliano.giusti@unibo.it>
2023-02-27 21:36:04 +0100

    add

--- rundocker.sh
+++ rundocker.sh
@@ -3,11 +3,11 @@ docker build -t nn  app
 docker stop NN
 docker image prune -f
 docker container prune -f
-docker run -t --name=NN --rm -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run.sh
+docker run -t --name=NN --rm -v $PWD/app/db:/home/node/app/db -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run.sh
 ./sync.sh
 for i in 1 2 3 4 5 6 7 8 9 10
 do
-docker run -t --name=NN --rm -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run-2.sh
+docker run -t --name=NN --rm -v $PWD/app/db:/home/node/app/db -v $PWD/app/dati:/home/node/app/dati -v $PWD/app/html:/home/node/app/html -v $PWD/app/output:/home/node/app/output nn bin/run-2.sh
 ./sync.sh
 done
 #docker run -t --name=NN --rm  nn 
