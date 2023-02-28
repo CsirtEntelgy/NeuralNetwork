337be7c2cdd32cc281ffbb43c48c46bf3cddb9b0
giuliano.giusti@unibo.it <giuliano.giusti@unibo.it>
2023-02-28 20:12:35 +0100

    json package

--- install/install.sh
+++ install/install.sh
@@ -3,3 +3,13 @@
 sudo apt-get install openssh gnuplot git wget nodejs npm sqlite
 git config --global user.email "giuliano.giusti61@gmail.com"
 git config --global user.name "giuliano giusti"
+cd
+(
+echo "#!/bin/bash"
+echo "cd NeuralNetwork"
+echo "git pull"
+echo "cd app"
+echo "npm install"
+echo "node base.js"
+)>runserver.sh 
+chmod +x runserver.sh
