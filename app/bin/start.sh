35067f20c66c7f3fd0109fa36f5fdf020edc0caa
giuliano giusti <giuliano.giusti61@gmail.com>
2023-02-28 22:18:45 +0100

    Aggiornamento Tue Feb 28 22:18:45 CET 2023

--- app/bin/start.sh
+++ app/bin/start.sh
@@ -5,6 +5,7 @@ rm -rf $WD_DB/
 mkdir -p ${WD_DB}
 
 cat bin/borsa.sql | sqlite3 $DB
+cat bin/dati.sql | sqlite3 $DB
 DB_RETI=$WD_DB/reti.db
 (
 echo "CREATE TABLE nn (id_azienda integer, chiave text,n1 integer, n2 integer, n3 integer, jsonData Text,primary key (id_azienda,chiave,n1,n2,n3));"
@@ -13,6 +14,6 @@ DB_ELAB=$WD_DB/elaborazioni.db
 (
 echo "CREATE TABLE iterazioni (id_azienda integer,chiave text,iter integer,errore number,primary key (id_azienda,chiave));"
 ) | sqlite3 $DB_ELAB
-set -e
+
 aggiornaDB
 
