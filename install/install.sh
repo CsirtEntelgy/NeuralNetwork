6db91fe8753104f046e7fbfb07f5ad8ed794f1e8
giuliano.giusti@unibo.it <giuliano.giusti@unibo.it>
2023-02-28 20:13:33 +0100

    json package

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #apk add bash openssh gnuplot git wget nodejs npm sqlite
-sudo apt-get install openssh gnuplot git wget nodejs npm sqlite
+sudo apt-get install gnuplot git wget nodejs npm sqlite
 git config --global user.email "giuliano.giusti61@gmail.com"
 git config --global user.name "giuliano giusti"
 cd
